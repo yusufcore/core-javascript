8d6f6f35de72ed8ca89c20ad8d668cd5e76548e5
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-04 20:01:53 +0530

    add if else statements

---
+++ controlflow/if_else.js
@@ -0,0 +1,16 @@
+// If statement
+
+const num = 5;
+
+if (num > 0){
+    console.log("The number is positive");
+}
+
+// If-Else statement
+
+let num1 = -10;
+
+if (num1 > 0){
+    console.log("The number is positive");
+}else
+    console.log("The number is negative");
