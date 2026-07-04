63d822e5a8b766d9cea75cd83bdfc34c1e7721c0
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-04 20:06:06 +0530

    add switch case

---
+++ controlflow/switch.js
@@ -0,0 +1,16 @@
+// Switch Case
+
+let num = 5;
+
+switch (num){
+    case 0:
+        console.log("Number is Zero. ");
+        break;
+    case 1:
+        console.log("Number is one.")
+        break;
+    case 2:
+        console.log("Number is Two. ");
+    default:
+        console.log("Number is greater than 2.")
+}
