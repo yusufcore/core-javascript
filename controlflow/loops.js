4f8b7a08b04c2458096e82f535900227070cd83b
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-04 20:14:08 +0530

    add loops in js

---
+++ controlflow/loops.js
@@ -0,0 +1,25 @@
+// For Loop
+
+for (let i = 1; i <= 10; i++){
+    if(i % 2 === 0){
+        console.log(i);  
+    }
+}  // program to print even numbers from 1 to 10;
+
+// While loop
+
+let i = 1;
+
+while(i <= 5){
+    console.log(i);
+    i++;
+}
+
+// Do-While loop
+
+let j = 1;
+
+do{
+    console.log(j);
+    j++;
+}while (j <= 5);
