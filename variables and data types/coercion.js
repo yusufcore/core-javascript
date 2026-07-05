86c449583e9dd1f90afbfc7fbc7ec72388cf2cd7
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-05 21:26:43 +0530

    add coercion

---
+++ variables and data types/coercion.js
@@ -0,0 +1,12 @@
+console.log("5" + 2);
+console.log("5" + true)
+console.log("5" - 2);
+console.log("5" * 2);
+console.log("10" / "2");
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(null + 1);
+
+console.log(NaN == NaN);  // false
+console.log(isNaN(NaN))   // true
