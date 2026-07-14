76210643a61d34f65a1db5c778ebf45812964a0f
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-14 08:23:53 +0000

    add function.js

---
+++ functions/basic.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("My name is John Doe");
+}
+sayMyName();
+
+function addTwoNumbers(number1, number2){  // number1 and number2 are called parameters.
+    let result = number1 + number2;
+    return result;
+}
+const result = addTwoNumbers(19, 20);
+console.log("Result: ", result);
+// after return statement, the function will stop executing and return the value to the caller. Any code after the return statement will not be executed.\
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("John Doe"));
